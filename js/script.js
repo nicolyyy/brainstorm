880768aa15c43ebb712bcb72893f34ec109cfe68
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-26 20:44:17 -0300

    fix gab
    adicionei o script do gabril e constei uma parte do visual

--- js/script.js
+++ js/script.js
@@ -1,37 +1,67 @@
 // JavaScript Document
 //favor não alterar
- $(document).ready(function() {
+$(document).ready(function () {
     $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
             $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
+        }
+    });
+});
 
-  //logica filtro portifolio
-  $('.btn').on('click', function(){
+//logica filtro portifolio
+$('.btn').on('click', function () {
     var categoria = $(this).attr('data-categoria')
     var todosCards = $('.cards-portfolio')
     $(todosCards).fadeIn(800)
 
-    if(categoria == "todos"){
+    if (categoria == "todos") {
         $(todosCards).show()
     }
-    
-    else{
-    $('.cards-portfolio').each(function(){
-        if(!$(this).hasClass(categoria)){
-          
-            $(this).hide()
-        }else{
-            
-            $(this).show()
-        }
+
+    else {
+        $('.cards-portfolio').each(function () {
+            if (!$(this).hasClass(categoria)) {
+
+                $(this).hide()
+            } else {
+
+                $(this).show()
+            }
         })
     }
-  })
+})
 
-  
+function collapse1() {
+    let check = document.getElementById("collapsible1");
+    let content = document.getElementById("card-content1");
+    if (check.checked == true) {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function collapse2() {
+    let check = document.getElementById("collapsible2");
+    let content = document.getElementById("card-content2");
+    if (check.checked == true) {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function collapse3() {
+    let check = document.getElementById("collapsible3");
+    let content = document.getElementById("card-content3");
+    if (check.checked == true) {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+
 
